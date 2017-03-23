76bff0757132af54e8a2da6c0708cc8f2f817461
Nitin Sharma <nitinsharma0389@gmail.com>
2017-03-22 22:49:11 -0500

    Signin and signup logic

--- hello.py
+++ hello.py
@@ -1,11 +1,27 @@
-from cloudant import Cloudant
-from flask import Flask, render_template, request, jsonify
-import atexit
-import cf_deployment_tracker
 import os
 import json
+import uuid
+import atexit
+import datetime
+import traceback
+import cf_deployment_tracker
 from flask_mail import Mail
+from functools import wraps
+from flask import g, request
+from cloudant import Cloudant
 from flask.ext.mail import Message
+from cloudant.result import Result
+from flask import Flask, render_template, request, jsonify
+
+# Decorators
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if g.user is None:
+            return render_template('signin.html', signin_needed = 'Signin before continuing')
+        return f(*args, **kwargs)
+    return decorated_function
 
 # Emit Bluemix deployment event
 cf_deployment_tracker.track()
@@ -17,13 +33,21 @@ MAIL_USE_SSL= True
 MAIL_USERNAME = os.getenv('GMAIL_USERNAME')
 MAIL_PASSWORD = os.getenv('GMAIL_PASSWORD')
 
+#Messages
+success_message = 'Email sent to {}. Please click the link given in the email to activate the account!'
+user_exists_message = 'The user with given email id is registered. Login to continue'
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 mail = Mail(app)
 
-db_name = 'mydb'
+db_name = 'users'
+keys_db_name = 'keys'
 client = None
 db = None
+keys_db = None
+
+# Utility functions
 
 def send_email(to, subject, template):
 	msg = Message(
@@ -33,10 +57,83 @@ def send_email(to, subject, template):
         sender="testloginapp111@gmail.com")
 	mail.send(msg)
 
+def generate_confirmation_token():
+	return uuid.uuid4().hex
+	
+def email_template(confirmation_url):
+	template = """
+		<p>Hi There! </p>
+		<p>Thank you for signing up. Please click the link below to activate your account:</p>
+		<p><a href="{}">{}</a></p>
+		<br>
+		<p>Cheers!</p>
+	"""
+	return template.format(confirmation_url, confirmation_url)
+
+def format_email(recipient, confirmation_token):
+	confirmation_url = 'login-testapp.mybluemix.net/confirm-account/{}'.format(confirmation_token)
+	email_content = email_template(confirmation_url)
+	subject = 'Please confirm your account!'
+	try:
+		send_email(recipient, subject, email_content)
+	except Exception:
+		print "Could not send email. Traceback:"
+		traceback.format_exc()
+
+def get_current_timestamp():
+	timestamp = '{:%m-%d-%Y %H:%M:%S}'.format(datetime.datetime.now())
+	return timestamp
+
+def create_user(email, confirmation_token, password, full_name):
+	"""
+		Fields:
+		_id = email
+		confirmed_acc = False (by default)
+		confirmation_token = generate_confirmation_token()
+		password
+		time_last_visited -> last 5 logins
+		full_name
+	"""
+	if client:
+		data = {
+        	'_id': email,
+        	'full_name': full_name,
+        	'password': password,
+        	'confirmation_token': confirmation_token,
+        	'time_last_login': get_current_timestamp(),
+        	'confirmed_account': 'False',
+        	}
+        new_document = db.create_document(data)
+        if new_document.exists():
+        	return True
+	return False
+
+def is_authenticated(email, password):
+	existing_user_entries = client['users']
+	if existing_user_entries[email]:
+		return existing_user_entries[email]['password'] == password
+	 
+
+def get_last_login_and_update_to_current(email):
+	existing_user_entries = client['users']
+	time_last_login = existing_user_entries[email]['time_last_login']
+	existing_user_entries[email]['time_last_login'] = get_current_timestamp()
+	return time_last_login
+
+def check_if_user_exists(email):
+	existing_user_entries = client['users']
+	for user_data in existing_user_entries:
+		print user_data
+		print user_data['_id']
+		if user_data['_id'] == email:
+			print 'Got there'
+			return True
+	return False
+	
+# Configs
 
 if 'VCAP_SERVICES' in os.environ:
     vcap = json.loads(os.getenv('VCAP_SERVICES'))
-    print vcap
     print('Found VCAP_SERVICES')
     if 'cloudantNoSQLDB' in vcap:
         creds = vcap['cloudantNoSQLDB'][0]['credentials']
@@ -45,7 +142,8 @@ if 'VCAP_SERVICES' in os.environ:
         url = 'https://' + creds['host']
         client = Cloudant(user, password, url=url, connect=True)
         db = client.create_database(db_name, throw_on_exists=False)
-        print('DB CREATED')
+        print('DB CONNECTED')
+
 elif os.path.isfile('vcap-local.json'):
     with open('vcap-local.json') as f:
         vcap = json.load(f)
@@ -61,9 +159,14 @@ elif os.path.isfile('vcap-local.json'):
 # When running this app on the local machine, default the port to 8080
 port = int(os.getenv('PORT', 8080))
 
+@app.before_request
+def before_request():
+    g.user = None
+
+#Views
+
 @app.route('/')
 def home():
-	send_email("nitinsharma0389@gmail.com", "subject", "template")
 	return render_template('signup.html')
 
 @app.route('/signup')
@@ -74,43 +177,29 @@ def signup():
 def signin():
     return render_template('signin.html')
 
-# /* Endpoint to greet and add a new visitor to database.
-# * Send a POST request to localhost:8080/api/visitors with body
-# * {
-# * 	"name": "Bob"
-# * }
-# */
-@app.route('/api/visitors', methods=['GET'])
-def get_visitor():
-    if client:
-    	print('got data')
-    	return jsonify(['got data'])
-#        return jsonify(list(map(lambda doc: doc['name'], db)))
-    else:
-        print('No database')
-        return jsonify(['No database'])
+@app.route('/new_user', methods=['POST'])
+def new_user():
+	email = request.form['email']
+	name = request.form['name']
+	password = request.form['password']
+	if check_if_user_exists(email):
+		print "User already exists. Redirecting to signin page"
+		return render_template('signin.html', user_exists_message= user_exists_message)
+	else:
+		print "Creating User with email : {}".format(email)
+		confirmation_token = generate_confirmation_token()
+		create_user(email, confirmation_token, password, name)
+		format_email(email, confirmation_token)
+		return render_template('success.html', success_message = success_message.format(email))
 
-# /**
-#  * Endpoint to get a JSON array of all the visitors in the database
-#  * REST API example:
-#  * <code>
-#  * GET http://localhost:8080/api/visitors
-#  * </code>
-#  *
-#  * Response:
-#  * [ "Bob", "Jane" ]
-#  * @return An array of all the visitor names
-#  */
-@app.route('/api/visitors', methods=['POST'])
-def put_visitor():
-    user = request.json['name']
-    if client:
-        data = {'name':request.json['name']}
-        db.create_document(data)
-        return 'Hello %s! I added you to the database.' % user
-    else:
-        print('No database')
-        return 'Hello %s!' % user
+@app.route('/login_user', methods=['POST'])
+def login_user():
+	email = request.form['email']
+	password = request.form['password']
+	if is_authenticated(email, password):
+		g.user = email
+		last_login_time = get_last_login_and_update_to_current(email)
+		return render_template('last_login.html', last_login_time = last_login_time)
 
 @atexit.register
 def shutdown():
